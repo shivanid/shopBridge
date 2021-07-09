206b12c24d6f986cb4c3e2734ae028bde67d4a46
Shivani Deshpande <shivani.deshpande@tieto.com>
2021-07-10 01:22:14 +0530

    commit changes

---
+++ DBScriptToCreaeTable.sql
@@ -0,0 +1,28 @@
+create Database Products
+
+Use Products
+
+SET ANSI_NULLS ON  
+GO  
+  
+SET QUOTED_IDENTIFIER ON  
+GO  
+  
+SET ANSI_PADDING ON  
+GO  
+  
+CREATE TABLE [dbo].[Items](  
+    [ItemID] [int] IDENTITY(1,1) NOT NULL,  
+    [ItemName] [varchar](50) NULL,  
+    [Description] [varchar](50) NULL,  
+	price [float](2) NULL,  
+PRIMARY KEY CLUSTERED   
+(  
+    [ItemID] ASC  
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]  
+) ON [PRIMARY]  
+  
+GO  
+  
+SET ANSI_PADDING OFF  
+GO  
